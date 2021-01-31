20413780721d81fe155b587f6eac8adf65568112
Oleg Ostrovski <oleg.ostrovski@gmail.com>
2021-01-31 12:04:58 +0300

    task dream-team is completed

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,16 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return false;
+  }
+
+  let nameArray = members.filter((x) => typeof x === "string");
+  let firstLettersOfNames = nameArray
+    .reduce((acc, item) => {
+      return acc.concat(item.trim()[0].toUpperCase());
+    }, [])
+    .sort()
+    .join("");
+  return firstLettersOfNames;
 };
