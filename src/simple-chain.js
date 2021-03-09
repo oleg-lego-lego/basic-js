19c0066a85f6d9bd52ce3c3ab5645d6347b00c8b
Oleg Ostrovski <oleg.ostrovski@gmail.com>
2021-03-09 21:21:25 +0300

    feat: add task simple-chain is completed

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,5 +1,33 @@
 const CustomError = require("../extensions/custom-error");
 
-const chainMaker = {};
+const chainMaker = {
+  chain: [],
+  getLength() {
+    return this.chain.length;
+  },
+  addLink(value = " ") {
+    this.chain.push(`( ${new String(value)} )`);
+    return this;
+  },
+  removeLink(position) {
+    if (position >= 1 && position <= this.getLength()) {
+      this.chain.splice(position - 1, 1);
+      return this;
+    } else {
+      this.chain = [];
+      throw new Error();
+    }
+  },
+  reverseChain() {
+    this.chain.reverse();
+    return this;
+  },
+  finishChain() {
+    let resultStr =
+      this.getLength() > 1 ? this.chain.join("~~") : this.chain[0];
+    this.chain = [];
+    return resultStr;
+  },
+};
 
 module.exports = chainMaker;
