8b5513337faf7966a83a2d9a1f9264a9ac893374
Oleg Ostrovski <oleg.ostrovski@gmail.com>
2021-03-09 21:16:34 +0300

    feat: add task transform-array is completed

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,32 +1,5 @@
 const CustomError = require("../extensions/custom-error");
 
-const chainMaker = {
-  chain: [],
-  getLength() {
-    return this.chain.length;
-  },
-  addLink(value = " ") {
-    this.chain.push(`( ${new String(value)} )`);
-    return this;
-  },
-  removeLink(position) {
-    if (position >= 1 && position <= this.getLength()) {
-      this.chain.splice(position - 1, 1);
-      return this;
-    } else {
-      this.chain = [];
-      throw new Error();
-    }
-  },
-  reverseChain() {
-    this.chain.reverse();
-    return this;
-  },
-  finishChain() {
-    let result = this.getLength() > 1 ? this.chain.join("~~") : this.chain[0];
-    this.chain = [];
-    return result;
-  },
-};
+const chainMaker = {};
 
 module.exports = chainMaker;
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,35 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) throw Error;
+
+  let newArray = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === "--double-next" && arr[i + 1] !== undefined) {
+      newArray.push(arr[i + 1]);
+    } else if (
+      arr[i] === "--double-prev" &&
+      arr[i - 1] !== undefined &&
+      arr[i - 2] !== "--discard-next"
+    ) {
+      newArray.push(arr[i - 1]);
+    } else if (arr[i] === "--discard-next" && arr[i + 1] !== undefined) {
+      i++;
+    } else if (
+      arr[i] === "--discard-prev" &&
+      arr[i - 1] !== undefined &&
+      arr[i - 2] !== "--discard-next"
+    ) {
+      newArray.pop();
+    } else if (
+      arr[i] !== "--double-next" &&
+      arr[i] !== "--double-prev" &&
+      arr[i] !== "--discard-next" &&
+      arr[i] !== "--discard-prev"
+    ) {
+      newArray.push(arr[i]);
+    }
+  }
+  return newArray;
 };
